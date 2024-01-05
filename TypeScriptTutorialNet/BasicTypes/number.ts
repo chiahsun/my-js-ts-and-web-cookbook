4335ef3a4ef43126a6ba3578c486c1beae38a22a
chiahsun <chiahsun0814@gmail.com>
2024-01-05 22:22:50 +0800

    Add typescript tutorial type number

---
+++ TypeScriptTutorialNet/BasicTypes/number.ts
@@ -0,0 +1,13 @@
+let price = 9.95;
+
+let counter2 = 0;
+
+let bin = 0b100;
+let bin2 = 0B010;
+
+let octal = 0o10;
+
+let hex = 0XAC;
+
+let bigNum = 9007199254740991n; // <1>
+console.log(bigNum);
