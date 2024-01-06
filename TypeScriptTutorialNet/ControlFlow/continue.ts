e3b21dfb076ae45d49f8a75b02f5c906a50c6623
chiahsun <chiahsun0814@gmail.com>
2024-01-06 19:51:22 +0800

    Add typescript tutorial control flow

---
+++ TypeScriptTutorialNet/ControlFlow/continue.ts
@@ -0,0 +1,6 @@
+// https://www.typescripttutorial.net/typescript-tutorial/typescript-continue/
+for (let i = 0; i < 9; ++i) {
+    if (i % 2)
+        continue;
+    console.log(i);
+}
--- adoc.mk
+++ adoc.mk
@@ -2,7 +2,9 @@
 .PHONY: adoc-build adoc-view adoc-watch
 
 adoc-build: $(ROOT)/dark.css $(ROOT)/highlight/highlight.min.js
-	asciidoctor -a stylesheet=$(ROOT)/dark.css -a highlightjsdir=$(ROOT)/highlight -v -t index.adoc
+	asciidoctor -a lang=en -a stylesheet=$(ROOT)/dark.css -a highlightjsdir=$(ROOT)/highlight -v -t -o index.html index.adoc
+	asciidoctor -a lang=zh_TW -a stylesheet=$(ROOT)/dark.css -a highlightjsdir=$(ROOT)/highlight -v -t -o index-zh_TW.html index.adoc
+
 
 adoc-view:
 	open index.html -a Safari
