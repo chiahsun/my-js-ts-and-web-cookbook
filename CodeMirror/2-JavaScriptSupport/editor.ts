3207cd220a3560912f468b1496352642f7fc0d8a
chiahsun <chiahsun0814@gmail.com>
2024-01-07 23:30:48 +0800

    Add code editor javascript support

--- CodeMirror/2-JavaScriptSupport/editor.ts
+++ CodeMirror/2-JavaScriptSupport/editor.ts
@@ -1,11 +1,11 @@
 import {basicSetup, EditorView} from "codemirror"
-import {javascript} from "@codemirror/lang-javascript"
+import {javascript} from "@codemirror/lang-javascript" // <1>
 
 
 export function setupEditor(parent: HTMLDivElement) {
   new EditorView({
     doc: "console.log('hello')\n",
-    extensions: [basicSetup, javascript()],
+    extensions: [basicSetup, javascript()], // <2>
     parent: parent
   })
 }
